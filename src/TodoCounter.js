9726d6cb11410ec82773ae5d28647c3a647b38ae
Edmara Guerrero <edxilcoil@gmail.com>
2023-11-23 07:58:29 -0500

    Agregar todos los componentes del todo list

---
+++ src/TodoCounter.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function TodoCounter(){
+    return(
+      /****ELEMENTOS */
+      <h1>Has completado 3 de 5 TODOs</h1>
+    );
+  }
+
+export { TodoCounter };
---
+++ src/TodoItem.js
@@ -0,0 +1,11 @@
+function TodoItem(){
+    return(
+      /****ELEMENTOS */
+      <li>
+        <span>V</span>
+        <span>Limpiar Cocina</span>
+        <span>X</span>
+      </li>
+    );
+  }
+  export {TodoItem };
