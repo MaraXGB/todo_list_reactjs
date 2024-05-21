a179c23ba1c9f65780debb1e1cdc433461778df6
Edmara Guerrero <edxilcoil@gmail.com>
2024-05-21 09:44:42 -0500

    usecontext de react para mejorar codigo y evitar props drill

--- src/TodoCounter/index.js
+++ src/TodoCounter/index.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import './TodoCounter.css'
+import { TodoContext } from '../TodoContext';
 // const styles={
 //   fontSize: '24px',
 //   textAlign: 'center',
 //   margin: '0',
 //   padding: '48px'
 // };
-function TodoCounter({ total, completed }){
+function TodoCounter(){
+  const {totalTodos,
+    completedTodos} = React.useContext(TodoContext);
     return(
       /****ELEMENTOS */ 
-      total == completed ?
+      totalTodos == completedTodos ?
         <h2>Felicidades!! has completado todos tus TODOs</h2>
         :
-        <h2>Has completado <span>{completed}</span> de <span>{total}</span> TODOs</h2>
+        <h2>Has completado <span>{completedTodos}</span> de <span>{totalTodos}</span> TODOs</h2>
     );
   }
 
--- src/TodoSearch/index.js
+++ src/TodoSearch/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './TodoSearch.css';
-function TodoSearch({
-  searchValue,
-  setSearchValue,
-}) 
+import { TodoContext } from '../TodoContext';
+function TodoSearch() 
 {
-return (
+  const {searchValue,
+    setSearchValue,} = React.useContext(TodoContext);
+  return (
     <div className='todo-search'>
       <span className='search-icon material-icons'>search</span>
       <input className='TodoSearch' placeholder='Lavar Ropa' value = {searchValue} onChange={(event) =>{
